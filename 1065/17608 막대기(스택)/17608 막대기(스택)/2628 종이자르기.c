8f5a89f6ffa9dc4902cf6e45b96161674da70108
hey-dudegg <m5rep5wer>
2024-03-29 11:16:20 +0900

    2628 종이 자르기 풀이 구상 완료 // 구현 남음

---
+++ 1065/17608 막대기(스택)/17608 막대기(스택)/2628 종이자르기.c
@@ -0,0 +1,18 @@
+﻿/* 
+나오는 조각-> 가로+1 * 세로+1
+각각의 조각들 크기 비교(max)
+*/
+
+#include <stdio.h>
+
+int main() {
+	int wid, hei, cut, max;
+	
+	scanf_s("%d %d", &wid, &hei);
+	scanf_s("%d", cut);
+
+
+
+
+	return 0;
+}
