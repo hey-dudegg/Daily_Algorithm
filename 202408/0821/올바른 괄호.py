cf8165263492f22870d5fbe4d92feaccbf44387f
hey-dude <154962837+hey-dudegg@users.noreply.github.com>
2024-09-22 00:30:35 +0900

    큐/스택 알고리즘 복습

--- 202408/0821/올바른 괄호.py
+++ 202408/0821/올바른 괄호.py
@@ -1,3 +1,57 @@
+'''
+[[복습]]
+
+문제 이해
+- (로 열렸으면, 반드시 동일한 갯수의 )로 닫혀야 한다.
+
+- 입력 : 잘 닫혔는지 모를 () 문자열
+- 출력 : 잘 닫혔다면 true, 아니라면 false
+
+문제 정의
+- ( -> +1, ) -> +0으로 생각한다.
+- cnt를 0으로 세고, -1이 되면 false 반환.
+
+- 문자열을 처음부터 탐색한다.
+ - 만약 cnt가 음수라면
+  - return false
+ - 아니라면
+  - ( -> +1, ) -> -1
+  
+ - if cnt != 0:
+  - return false
+ - else:
+  - return true
+
+
+제한 사항
+n = 100T
+O(n^2) 불가
+)로 시작하는 상황 불가, (이 시작점이다.
+
+느낀점
+반드시 스택을 사용해야 하는 것이 아니라, 스택의 원리를 활용해 리스트로 푸는 것이다.
+
+'''
+
+def solution(str):
+    
+    cnt = 0
+    
+    for c in str:
+        if cnt < 0:
+            return False
+        else:
+            if c == '(':
+                cnt += 1
+            else :
+                cnt -= 1
+    
+    if cnt == 0:
+        return True
+    else :
+        return False
+
+
 """
 문제 이해
 - 올바른 괄호 -> ()의 짝이 맞음
--- 202408/0821/프로세스.py
+++ 202408/0821/프로세스.py
@@ -1,3 +1,73 @@
+'''
+[[복습]]
+문제 이해
+큐에서 프로세스를 빼는데, 대기큐 안에 우선순위가 더 높다면 다시 넣는다.
+우선순위가 가장 높다면, 그대로 빠진다.
+순서 지킨다.(큐 사용)
+
+입력 : 우선순위 리스트, 알고싶은 숫자의 순서
+출력 : 해당 순서의 실행 순서
+
+문제 정의
+- 완전 탐색 두번 한다.
+- 순서를 알기 위한 cnt 순서 정의
+
+- dequeue한다.
+- dequeue한 값과 리스트의 모든 숫자와 비교한다.
+ - 값이 큰 값이 있다면 다시 enqueue한다.
+- 없다면
+ - continue
+ - cnt + 1
+
+return cnt
+
+제한 사항
+N = 100
+O(n^2) = 10T
+O(n^3) = 1M
+
+느낀점
+from collections import deque
+que = deque() -> 디큐 선언
+enumerate() -> 새 정렬된 리스트를 반환합니다.
+pop(), popleft() -> 삭제
+append(), appendleft() -> 삽입
+any() -> 하나라도 true면 true
+
+'''
+from collections import deque
+
+def solution(priorities, location):
+    # [2, 1, 3, 2]
+    # [0:2, 1:1, 2:3, 3:2]
+    
+    # 큐 채우기
+    queue = deque([]) # (loc, pri)
+    for loc, pri in enumerate(priorities):
+        queue.append((loc, pri))
+    
+
+    # - queue에서 프로세스 하나 꺼내기 (dequeue)
+    # - 대기중인 queue 조회
+    #   - 우선순위가 높은 프로세스가 있으면 (enqueue)
+    #   - 없으면 실행하기  
+    # - location에 해당하는 프로세스가 몇번째로 실행되는지 확인하기
+    
+    order = 0
+    
+    while queue:
+        cur = queue.popleft()
+        
+        if any(cur[1] < q[1] for q in queue):
+        # if cur_priority < max(queue_priority):
+            queue.append(cur)
+        else:
+            order += 1
+            if cur[0] == location:
+                return order
+
+
+
 """
 문제 이해
 - queue에서 프로세스 하나 꺼내기 (deque)
